10eb325bf700328c1c9c983eca2de67dc9d009ce
Dave Dash <dave@pinterest.com>
2013-06-10 18:00:23 +0000

    Better install

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
 cd ~/.dotfiles
 for f in * .*; do
     if [ "$f" = ".git" ]; then
+	continue
+    fi
+    # No files ending in "." e.g. . or ..
+    if [[ "$f" == *. ]]; then
+	continue
+    fi
+    if [[ "$f" != .* ]]; then
 	continue
     fi
     ln -s ~/.dotfiles/$f ~/$f
